c07e2f9dfadb2e410bce625e82503572dd8afe27
Luodoudou <echoscope@qq.com>
2020-07-10 05:11:16 +0800

    Update and rename JavaBitOperationMethods.java to NumberOperationMethods.java

--- NumberOperationMethods.java
+++ NumberOperationMethods.java
@@ -1,5 +1,75 @@
 
-public class JavaBitOperationMethods {
+public class NumberOperationMethods {
+	
+	/**
+	 * Binary to Hexadecimal
+	 * <p>
+	 * Convert a binary bits expression to a hexadecimal character
+	 * 
+	 * @param fourBits The four binary bits representation
+	 * @return The Hexadecimal character
+	 */
+	public static char binaryToHexadecimal(byte[] fourBits) {
+		int decimal = Main.binaryToDecimal(fourBits);
+		switch (decimal) {
+		case 0:
+			return '0';
+		case 1:
+			return '1';
+		case 2:
+			return '2';
+		case 3:
+			return '3';
+		case 4:
+			return '4';
+		case 5:
+			return '5';
+		case 6:
+			return '6';
+		case 7:
+			return '7';
+		case 8:
+			return '8';
+		case 9:
+			return '9';
+		case 10:
+			return 'A';
+		case 11:
+			return 'B';
+		case 12:
+			return 'C';
+		case 13:
+			return 'D';
+		case 14:
+			return 'E';
+		case 15:
+			return 'F';
+		default:
+			return '\0';
+		}
+	}
+	
+	/**
+	 * Binary to Decimal
+	 * <p>
+	 * Convert a binary bits representation to decimal number
+	 * 
+	 * @param bits The binary bits representation
+	 * @return The converted decimal number
+	 */
+	public static int binaryToDecimal(byte[] bits) { // Weight Ascend
+		int decimal = 0;
+		int i = 0;
+		while (i < bits.length) {
+			if (bits[i] == 0) {
+				// Do nothing
+			} else {
+				decimal += Math.pow(2, bits.length - 1 - i);
+			}
+			i++;
+		}
+		return decimal;
+	}
 
     public static void bitOperationExample() {
     
